1c96473d6e8e8a04446738f4a0c8a4541a2d3ef6
Rachel Thomas <rachel@fast.ai>
2016-12-20 17:37:33 -0800

    added folder of setup shell scripts

---
+++ setup/aws-alias.sh
@@ -0,0 +1,7 @@
+alias aws-get-p2='export instanceId=`aws ec2 describe-instances --filters "Name=instance-state-name,Values=stopped,Name=instance-type,Values=p2.xlarge" --query "Reservations[0].Instances[0].InstanceId"` && echo $instanceId'
+alias aws-get-t2='export instanceId=`aws ec2 describe-instances --filters "Name=instance-state-name,Values=stopped,Name=instance-type,Values=t2.large" --query "Reservations[0].Instances[0].InstanceId"` && echo $instanceId'
+alias aws-start='aws ec2 start-instances --instance-ids $instanceId && aws ec2 wait instance-running --instance-ids $instanceId && export instanceIp=`aws ec2 describe-instances --filters "Name=instance-id,Values=$instanceId" --query "Reservations[0].Instances[0].PublicIpAddress"` && echo $instanceIp'
+alias aws-ip='export instanceIp=`aws ec2 describe-instances --filters "Name=instance-id,Values=$instanceId" --query "Reservations[0].Instances[0].PublicIpAddress"` && echo $instanceIp'
+alias aws-ssh='ssh -i ~/.ssh/aws-key.pem ubuntu@$instanceIp'
+alias aws-stop='aws ec2 stop-instances --instance-ids $instanceId'
+export instanceId=i-9aa9c282
