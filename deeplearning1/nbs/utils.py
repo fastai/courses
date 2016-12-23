30ff3e1cc0b8b0d9d4889e7f509407e7dde398be
Jeremy Howard <j@fast.ai>
2016-12-23 11:26:44 -0800

    remove unused imports

--- deeplearning1/nbs/utils.py
+++ deeplearning1/nbs/utils.py
@@ -12,7 +12,6 @@ from itertools import chain
 import pandas as pd
 import PIL
 from PIL import Image
-import cv2
 from numpy.random import random, permutation, randn, normal, uniform, choice
 from numpy import newaxis
 import scipy
@@ -46,7 +45,6 @@ from keras.optimizers import SGD, RMSprop, Adam
 from keras.utils.layer_utils import layer_from_config
 from keras.metrics import categorical_crossentropy, categorical_accuracy
 from keras.layers.convolutional import *
-from keras.callbacks import ReduceLROnPlateau
 from keras.preprocessing import image, sequence
 from keras.preprocessing.text import Tokenizer
 
