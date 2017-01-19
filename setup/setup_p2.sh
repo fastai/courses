46af17e754a6e2c7bc3625ae5d2cb9899c16790d
James Puderer <jhpuderer@gmail.com>
2017-01-19 08:49:19 -0500

    Added some error checking to setup scripts

--- setup/setup_p2.sh
+++ setup/setup_p2.sh
@@ -5,6 +5,17 @@
 export name="fast-ai"
 export cidr="0.0.0.0/0"
 
+hash aws 2>/dev/null
+if [ $? -ne 0 ]; then
+    echo >&2 "'aws' command line tool required, but not installed.  Aborting."
+    exit 1
+fi
+
+if [ -z "$(aws configure get aws_access_key_id)" ]; then
+    echo "AWS credentials not configured.  Aborting"
+    exit 1
+fi
+
 # get the correct ami
 export region=`aws configure get region`
 if [ $region = "us-west-2" ]; then 
