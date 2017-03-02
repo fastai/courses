a5eef021e062ec9920aefeb979212e6384ea0eb6
Inanc Gumus <inanc@pixenka.com>
2017-03-02 16:07:09 +0300

    Add: Documentation for OpenSSL helper scripts

--- deeplearning1/nbs/init.sh
+++ deeplearning1/nbs/init.sh
@@ -1,5 +1,18 @@
 #!/usr/bin/env bash
-#Code adapted from https://gist.githubusercontent.com/rashmibanthia/5a1e4d7e313d6832f2ff/raw/1f32274758851a32444491500fef4852496596ce/jupyter_notebook_ec2.sh
+
+#
+# README:
+#
+# This script creates openssl certificates which are necessary for
+# publishing Jupyter notebooks through openssl when using EC2 instances.
+#
+# And it also configures Jupyter to use the created cerficates.
+#
+# NOTE: Your Jupyter notebook will be listening port number 8888.
+#       This is also will be configured by this script.
+#
+
+# Code adapted from https://gist.githubusercontent.com/rashmibanthia/5a1e4d7e313d6832f2ff/raw/1f32274758851a32444491500fef4852496596ce/jupyter_notebook_ec2.sh
 
 cd ~
 
